a0423fb63e4190b5addc655ceee853897ded9822
rayees-stellosys <64066377+rayees-stellosys@users.noreply.github.com>
2022-11-04 17:33:57 +0530

    Update AppDemoAnimatedGif.js

--- src/demo/AppDemoAnimatedGif.js
+++ src/demo/AppDemoAnimatedGif.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import Camera, { FACING_MODES } from '../lib';
 import './reset.css';
+import axios from 'axios';
 
 function App (props) {
   function handleTakePhoto (dataUri) {
-    // Do stuff with the photo...
-    console.log('takePhoto');
-    if ('geolocation' in navigator) {
-      alert('Yes');
-    } else {
-      alert('No');
-    }
-    alert(dataUri);
     navigator.geolocation.getCurrentPosition(function (position) {
-      alert(position.coords.latitude);
+      savePhotoDetails(position.coords, dataUri);
     });
-  }
+  };
+  function savePhotoDetails (coords, dataUri) {
+    let inputData = {
+      'Latitude': coords.latitude,
+      'Longitude': coords.longitude,
+      'ImageUri': dataUri
+    };
+
+    let apiURL = `https://hk0euugzdg.execute-api.ap-southeast-1.amazonaws.com/production/takePhoto/createPhoto`;
+    return new Promise((resolve, reject) => {
+      axios.post(apiURL, inputData).then((response) => {
+        console.log('response', response);
+        resolve(response);
+      }).catch((err) => {
+        console.log('err', err);
+        reject(err);
+      });
+    });
+  };
 
   return (
     <div>
